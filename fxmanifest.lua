d722198de7366d7a6580d3cfb027cc4cac5dfadd
Outsider <87246847+outsider31000@users.noreply.github.com>
2022-05-09 19:01:47 +0100

    RedM

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,7 @@
------------------------------------
---- Discord ACE Perms by Badger ---
------------------------------------
 
 fx_version 'cerulean'
-game 'gta5'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 server_script "config.lua"
 server_script "server.lua"
